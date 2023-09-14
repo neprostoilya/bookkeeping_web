4127087411538fd8b3cc1694527b041dba6f9f41
neprostoilya <geniumobile@gmail.com>
2023-09-15 02:35:20 +0500

    Completed: - Кнопка крестик TODO: - Доделать иконки в таблице

--- bookkeeping_site/static/bookkeeping/js/sort.jquery.js
+++ bookkeeping_site/static/bookkeeping/js/sort.jquery.js
@@ -8,16 +8,35 @@ $(document).ready(function() {
     var sortHref = $(this).attr('href')
     if (sortHref == '?sort=-' + sortValue) {
       sortHref = '?sort=' + sortValue;
+      $(this).append('<i style="margin-top: .2em;" class="fa fa-sort-alpha-up"></i>');
       history.pushState(null, null, sortHref);
     } else {
       sortHref = '?sort=-' + sortValue;
+      $(this).append('<i style="margin-top: .2em;" class="fa fa-sort-alpha-down"></i>');
       history.pushState(null, null, sortHref);
     }
     window.history.pushState(null, null, sortHref);
-    $(this).attr('href', sortHref);
+
     
     if (window.location.href != currentUrl) { 
       location.reload(); 
     }
+    $('a.remove-sort-link').addClass('d-none');
+    $(this).siblings('.remove-sort-link').removeClass('d-none');
   });
-});
+  
+  $('a.remove-sort-link').click(function(e) {
+    e.preventDefault();
+    var sortHref = window.location.href.replace(/[?&]sort=[^&]+/, '');
+    history.pushState(null, null, sortHref);
+    location.reload(); 
+  }); 
+
+  $('a.remove-sort-link').addClass('d-none');
+  var urlParams = new URLSearchParams(window.location.search);
+  var sortParam = urlParams.get('sort');
+  if (sortParam) {
+    var sortValue = sortParam.replace('-', '');
+    var $sortLink = $('a.sort-link[value="' + sortValue + '"]');
+    $sortLink.siblings('.remove-sort-link').removeClass('d-none');
+}});
